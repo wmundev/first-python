2495915ca57c469c7ba1288725b34cd3a5b2aaac
Wilson Mun <30316250+wilsonmun@users.noreply.github.com>
2017-12-10 12:40:39 +1100

    hello world v1

---
+++ HelloWorld.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
+
+f = 6
+print(f)
+
+print(("wow") + str(123))
+
+def function():
+    global f
+    f = "def"
+    print(f)
+
+
+function()
+print(f)
